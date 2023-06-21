01d18002442c0f30a7edc46f5eb92dfd8e8d2822
Davi <davi_martins2009@outlook.com>
2023-06-21 16:09:18 -0300

    Criando o prjeto e definincdo Rotas

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import{BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Home from "./pages/home";
+import Filme from "./pages/Filmes";
+
+function RouteApp(){
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/filme/:id" element={<Filme/>}/>
+        </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RouteApp;
