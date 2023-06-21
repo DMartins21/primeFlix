bbff437d67a4be4f20ce3a85976d153c99fa7905
Davi <davi_martins2009@outlook.com>
2023-06-21 16:53:04 -0300

    Criando Header da pagina

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import{BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Home from "./pages/home";
 import Filme from "./pages/Filmes";
+import Header from "./components/Header";
 
 function RouteApp(){
     return(
         <BrowserRouter>
+        <Header/>
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/filme/:id" element={<Filme/>}/>
